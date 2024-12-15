23e606c8ed3f9549a8f7abe7e12e62df8d24521c
Muaaz Ahmed Baig <muaazahmed0111@gmail.com>
2024-12-15 13:40:22 +0500

    React Lecture 1

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import  React from "react";
+import ReactDom from "react-dom/client";
+
+let rootDiv = document.getElementById('root');
+ReactDom.createRoot(rootDiv).render(
+  <div>
+    <h1>hello world</h1>
+    <h1>hello react</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nobis atque, consequatur non in accusamus inventore neque voluptatem quo asperiores.</p>
+    <img width="250" src="vite.svg" alt="" />
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s" alt="" />
+
+  </div>
+)
